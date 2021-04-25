33d0622d4f8e39c242eefb65bd95875329526d43
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-04-25 17:31:07 -0500

    nuevo loguni

--- app/Http/Controllers/Versiondos/Login/LoginController.php
+++ app/Http/Controllers/Versiondos/Login/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Versiondos\Login;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Auth;
 use App\User;
 
@@ -11,6 +12,10 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        $respuesta = true;
+        $mensaje = "Bienvenido, ";
+        $datos = [];
+
         $username   = $request->usuario;
         $contrasena = $request->contrasenia;
 
@@ -19,33 +24,35 @@ class LoginController extends Controller
             'contrasenia'    => 'required',
         ]);
         
-        $usuario = User::where('username', $username)
-                        ->first();
+        $usu = User::where('username', $username)
+                    ->first([
+                        'id as usuid',
+                        'username as usuusuario',
+                        'name as pernombre',
+                        'email',
+                        'imagen',
+                        'remember_token as usutoken'
+                    ]);
+
+        if($usu){
+            if (Hash::check($contrasena, $usu->password)) {
+
+                $mensaje = "Bienvenido, ".$usu->username." es un gusto volver a verte por aquí";
+                $datos = $usu;
+
+            }else{
+                $respuesta = false;
+                $mensaje = "Lo sentimos, el usuario o contraseña es incorrecta";
+            }
+        }else{
+            $respuesta = false;
+          
+        }
 
         return response()->json([
-            'respuesta' => true,
-            'mensaje'   => "mensaje correcto",
-            'datos'     => [],
+            'respuesta' => $respuesta,
+            'mensaje'   => $mensaje,
+            'datos'     => $datos,
         ]);
-
-    //     if($usuario){
-    //         if (Auth::attempt(['username' => $username, 'password' => $contrasena])) {
-    //             $user = $this->guard()->user();
-    //             return redirect()->intended($this->redirectPath());
-                
-    //         }else{
-    //             return redirect()->back()
-    //             ->withInput()
-    //             ->withErrors([
-    //                 'login' => 'These credentials do not match our records.',
-    //             ]);
-    //         }
-    //     }else{
-    //         return redirect()->back()
-    //             ->withInput()
-    //             ->withErrors([
-    //                 'login' => 'These credentials do not match our records.',
-    //             ]);
-    //     }
     }
 }
