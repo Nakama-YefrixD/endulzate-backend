c95e3683c3764f04f5b12154372c3b9823461310
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-05-03 00:24:54 -0500

    cargar

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'name', 'email', 'password', 'imagen', 'username'
+        'id', 'name', 'email', 'password', 'imagen', 'username', 'remember_token'
     ];
 
     /**
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password'
     ];
 
     /**
